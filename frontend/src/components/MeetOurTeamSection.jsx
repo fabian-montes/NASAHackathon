0ff38cdea21553ca5ec2352f44ee46ab7502f3c4
fabianmontes <fabianmontes11fab@gmail.com>
2025-10-04 21:52:29 -0600

    about us con efecto hover

--- frontend/src/components/MeetOurTeamSection.jsx
+++ frontend/src/components/MeetOurTeamSection.jsx
@@ -3,36 +3,50 @@ import React from 'react';
 import ProfileCard from './ProfileCard'; // Assuming ProfileCard is in the same directory
 import '../css/MeetOurTeamSection.css'; // For basic styling if you want
 
+import planetA from '../assets/planetA.jpg';
+import planetB from '../assets/planetB.jpg';
+import planetC from '../assets/planetC.jpg';
+import planetD from '../assets/planetD.jpg';
+
+// import aylinFile from './assets/aylin.jpg';
+// import karlaFile from './assets/karla.jpg';
+// import adyleneFile from './assets/adylene.jpg';
+// import blancaFile from './assets/blanca.jpg';
+// import fabianFile from './assets/fabian.jpg';
+// import juanFile from './assets/juan.jpg';
+
+import silhouette from '../assets/silhouette.png';
+
 const teamMembers = [
   {
     name: "Aylin Ximena Ocampo Vera",
-    backgroundFile: "path/to/aylin-background.png",
-    mainImageFile: "path/to/aylin-silhouette.png",
+    backgroundFile: planetD,
+    mainImageFile: silhouette
   },
   {
     name: "Karla Irais De Florencio Romero",
-    backgroundFile: "path/to/karla-background.png",
-    mainImageFile: "path/to/karla-silhouette.png",
+    backgroundFile: planetB,
+    mainImageFile: silhouette
   },
   {
     name: "Adylene Baylon Cuahutlapantzi",
-    backgroundFile: "path/to/adylene-background.png",
-    mainImageFile: "path/to/adylene-silhouette.png",
+    backgroundFile: planetA,
+    mainImageFile: silhouette
   },
   {
     name: "Blanca Flor Visca Cocotzin",
-    backgroundFile: "path/to/blanca-background.png",
-    mainImageFile: "path/to/blanca-silhouette.png",
+    backgroundFile: planetB,
+    mainImageFile: silhouette
   },
   {
     name: "Fabián Márquez Montes",
-    backgroundFile: "path/to/fabian-background.png",
-    mainImageFile: "path/to/fabian-silhouette.png",
+    backgroundFile: planetC,
+    mainImageFile: silhouette
   },
   {
     name: "Juan Diego Vázquez Cabrera",
-    backgroundFile: "path/to/juan-diego-background.png",
-    mainImageFile: "path/to/juan-diego-silhouette.png",
+    backgroundFile: planetA,
+    mainImageFile: silhouette
   },
 ];
 
